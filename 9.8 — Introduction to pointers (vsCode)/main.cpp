9b4758fbae2ed8b95ffa79ff9b2aa1068a04e5f2
Mateusz Kacperski <oureyelet@gmail.com>
2021-05-12 17:59:29 +0100

    address-of (&) and indirection (*)

--- 9.8 — Introduction to pointers (vsCode)/main.cpp
+++ 9.8 — Introduction to pointers (vsCode)/main.cpp
@@ -5,5 +5,17 @@ int main()
 {
     print("Hello World");
 
+    ///////////////////////////////////////
+    //     The address-of operator (&)
+    ///////////////////////////////////////
+    int x{ 5 };
+    std::cout << x << '\n';// print the value of variable x
+    std::cout << &x << '\n';// print the memory address of variable x
+
+    ///////////////////////////////////////
+    //     The indirection operator (*)
+    ///////////////////////////////////////
+    
+
     return 0;
 }
