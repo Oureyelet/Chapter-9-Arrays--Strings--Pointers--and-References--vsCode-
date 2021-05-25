7e5ef998d6ee8a925fd34aec97c466f522660651
Mateusz Kacperski <oureyelet@gmail.com>
2021-05-25 19:14:31 +0100

    Pointers and const

--- 9.15 — Pointers and const (vsCode)/main.cpp
+++ 9.15 — Pointers and const (vsCode)/main.cpp
@@ -50,7 +50,32 @@ int main()
     //////////////////////////////////////////////////////////////////
     std::cout << std::endl;
     
+    int valuee{ 77 };
+    const int* const ptrr{ &valuee };
 
+    //////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////" << '\n';
+    std::cout << "Recapping" << '\n';
+    std::cout << "/////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////
+    std::cout << std::endl;
+
+    //To summarize, you only need to remember 4 rules, and they are pretty logical:
+    /*
+    -A non-const pointer can be redirected to point to other addresses.
+    -A const pointer always points to the same address, and this address can not be changed. 
+
+    -A pointer to a non-const value can change the value it is pointing to. 
+    These can not point to a const value.
+    -A pointer to a const value treats the value as const (even if it is not), and 
+    thus can not change the value it is pointing to. 
+    */
+    /*
+    int value{ 5 };
+    const int* ptr1{ &value }; // ptr1 points to a "const int", so this is a pointer to a const value.
+    int* const ptr2{ &value }; // ptr2 points to an "int", so this is a const pointer to a non-const value.
+    const int* const ptr3{ &value }; // ptr3 points to a "const int", so this is a const pointer to a const value.
+    */
 
     
 
