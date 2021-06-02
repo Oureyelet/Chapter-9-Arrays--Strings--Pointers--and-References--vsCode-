5cb485c25f5be680a25817abe988522495295ded
Oureyelet <oureyelet@gmail.com>
2021-06-02 21:02:59 +0100

    Quiz TIME !

---
+++ 9.17 — References and const (vsCode)/question_1.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+
+int main()
+{
+    std::cout << "Quiz Time!" << '\n';
+
+    /*
+    Which of the following types should be passed by value, 
+    which by const reference? You can assume that the function which takes these types 
+    as parameters doesn’t modify them.
+
+    a) char // NO
+    b) std::string // NO
+    c) unsigned long // NO
+    d) bool // NO
+    e) An enumerator // NO
+
+    f) // NO
+    struct Position
+    {
+        double x{};
+        double y{};
+        double z{};
+    };
+
+    g) // NO
+    struct Player
+    {
+        int health{};
+        // The Player struct is still under development. More members will be added.
+    };
+
+    h) double // NO
+    
+    i)  
+    struct ArrayView // NO
+    {
+        const int* array{};
+        std::size_t length{};
+    };
+    */
+
+    return 0;
+}
