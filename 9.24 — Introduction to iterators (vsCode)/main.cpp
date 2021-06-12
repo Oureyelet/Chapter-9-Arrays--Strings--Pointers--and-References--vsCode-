526481eb5f74fd2686ee9d9f4b09e90102b05348
Oureyelet <oureyelet@gmail.com>
2021-06-12 22:11:47 +0100

    iterator / begin / end / auto / std::array

--- 9.24 — Introduction to iterators (vsCode)/main.cpp
+++ 9.24 — Introduction to iterators (vsCode)/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array> // for std::array
 #include <cstddef> //for size_t
+#include <iterator> //for std::begin / std::end
 
 int main()
 {
@@ -9,7 +10,7 @@ int main()
     std::cout << std::endl;
     //////////////////////////////////////////////////////////////////////////////////////////
     std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
-    std::cout << "iterating" << '\n';
+    std::cout << "Introduction" << '\n';
     std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
     //////////////////////////////////////////////////////////////////////////////////////////
     /*
@@ -52,6 +53,89 @@ int main()
         std::cout << dt << ' ';
      }
      std::cout << '\n';
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "iterators" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    An iterator is an object designed to traverse through a container 
+    (e.g. the values in an array, or the characters in a string), providing access to each 
+    element along the way.
+    */
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Pointers as an iterator" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    The simplest kind of iterator is a pointer, which (using pointer arithmetic) works for data 
+    stored sequentially in memory. Let’s revisit a simple array traversal using a pointer and 
+    pointer arithmetic:
+    */
+
+    std::array ourArray { 0, 1, 2, 3, 4, 5, 6 };
+
+    auto begin { &ourArray[0] };
+    // note that this points to one spot beyond the last element
+    auto end { begin + std::size(ourArray) };
+
+    // for-loop with pointer
+    for(auto ptr{ begin }; ptr != end; ++ptr)// ++ to move to next element
+    {
+        std::cout << *ptr << ' ';// Indirection to get value of current element
+    }
+    std::cout << std::endl;
+
+    
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Standard library iterators" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Iterating is such a common operation that all standard library containers offer direct support 
+    for iteration. Instead of calculating our own begin and end points, we can simply ask the 
+    container for the begin and end points via functions conveniently named begin() and end():
+    */
+    std::array secondArray { 1, 2, 3 };
+
+    // Ask our array for the begin and end points (via the begin and end member functions).
+    auto begin_2{ secondArray.begin() };
+    auto end_2{ secondArray.end() };
+
+    for(auto p{ begin_2 }; p != end_2; ++p)// ++ to move to next element.
+    {
+        std:: cout << *p << ' ';// Indirection to get value of current element.
+    }
+    std::cout << '\n';
+
+    //The iterator header also contains two generic functions (std::begin and std::end) that can be used:
+
+    std::array thirdArray { 1, 2, 3 };
+
+    // Use std::begin and std::end to get the begin and end points.
+    auto begin_3{ std::begin(thirdArray) };
+    auto end_3{ std::end(thirdArray) };
+
+    for(auto p{ begin_3 }; p != end_3; ++p)// ++ to move to next element.
+    {
+        std:: cout << *p << ' ';// Indirection to get value of current element.
+    }
+    std::cout << '\n';
+
+    /*
+    Don’t worry about the types of the iterators for now, we’ll re-visit iterators in a later chapter. 
+    The important thing is that the iterator takes care of the details of iterating through the container. 
+    All we need are four things: the begin point, the end point, operator++ to move 
+    the iterator to the next element (or the end), and operator* to get the value of the current element.
+    */
+
 
 
 
